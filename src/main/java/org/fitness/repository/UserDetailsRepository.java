7d0945021c0a1bddd67cee9bb374bcffd996bfe8
Lionel Pinto <reachlion@gmail.com>
2023-03-06 09:29:37 -0500

    missed annotation

--- src/main/java/org/fitness/repository/UserDetailsRepository.java
+++ src/main/java/org/fitness/repository/UserDetailsRepository.java
@@ -2,6 +2,8 @@ package org.fitness.repository;
 
 import org.fitness.model.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
 }
